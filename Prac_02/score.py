cf52a7cb5a3e62030c4aa8be9912402fc684feb0
Jasmin07 <jasminkaur.kahlon@my.jcu.edu.au>
2023-03-14 16:19:07 +1000

    getting score

---
+++ Prac_02/score.py
@@ -0,0 +1,25 @@
+# BROKEN SCORE
+import random
+
+
+def main():
+    score = int(input("Enter score: "))
+    print(display_score_status(score))
+
+    random_score = random.randint(0, 100)
+    result = display_score_status(random_score)
+    print(f"Random score: {random_score} and the result: {result}")
+
+
+def display_score_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
